a96e9e60d36cae2ed1737fb8ea27014a783b59b7
Vlad <19valdis94@gmail.com>
2022-05-03 00:21:26 +0300

    next level

--- script.js
+++ script.js
@@ -23,20 +23,150 @@
 
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
-    personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    },
-    lastMovies = prompt('Один из последних просмотренных фильмов?', ''),
-    grade = prompt('На сколько оцените его?', ''),
-    lastMovies2 = prompt('Один из последних просмотренных фильмов?', ''),
-    grade2 = prompt('На сколько оцените его?', '');
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+//     personalMovieDB = {
+//         count: numberOfFilms,
+//         movies: {},
+//         actors: {},
+//         genres: [],
+//         privat: false
+//     },
+//     lastMovies = prompt('Один из последних просмотренных фильмов?', ''),
+//     grade = prompt('На сколько оцените его?', ''),
+//     lastMovies2 = prompt('Один из последних просмотренных фильмов?', ''),
+//     grade2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastMovies] = grade;
-personalMovieDB.movies[lastMovies2] = grade2; 
+// personalMovieDB.movies[lastMovies] = grade;
+// personalMovieDB.movies[lastMovies2] = grade2; 
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// console.log( NaN || 2 || undefined );
+ 
+// console.log( NaN && 2 && undefined );
+ 
+// console.log( 1 && 2 && 3 );
+ 
+// console.log( !1 && 2 || !3 );
+ 
+// console.log( 25 || null && !3 );
+ 
+// console.log( NaN || null || !3 || undefined || 5);
+ 
+// console.log( NaN || null && !3 && undefined || 5);
+ 
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// } else {
+//     console.log('Нихуя!');
+// } 
+
+// let num = 50;
+
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+// do {
+//     console.log(num);
+//     num++; 
+// }
+// while (num < 55);
+
+// let result = '';
+// const Length = 7;
+
+// for (let i = 1; i < Length; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+//     result += '\n';
+// }
+
+// console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) {
+                continue first;
+            }    
+            console.log(`Third level: ${k}`);
+        }
+    }        
+}
+
+for (let i = 5; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 20; i >= 10; i--) {
+    if (i === 13) {
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 2; i <= 10; i++) {
+    if (i%2 === 1) {
+        console.log(i);
+    }
+}
+
+// let i = 2;
+
+// while (i <= 16) {
+//     if (i % 2 === 0) {
+//         i++;
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+//     i++;
+// }
+// let i = 2;
+// while (i <= 16) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+// let arrayOfNumbers = [];
+// for (let i = 5; i <= 10; i++) {
+//     arrayOfNumbers[i - 5] = i;
+// }
+// console.log(arrayOfNumbers);
+
